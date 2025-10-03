0abee99d64f30f56c65bbbb760db70cf912e9e65
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-03 20:12:14 +0200

    prepare 1.21.9 port

---
+++ 1.21.9/Common/src/main/java/fuzs/hotbarslotcycling/impl/config/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package fuzs.hotbarslotcycling.impl.config;
+
+import org.jetbrains.annotations.ApiStatus;
---
+++ 1.21.9/NeoForge/src/main/java/fuzs/hotbarslotcycling/neoforge/impl/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package fuzs.hotbarslotcycling.neoforge.impl;
+
+import org.jetbrains.annotations.ApiStatus;
